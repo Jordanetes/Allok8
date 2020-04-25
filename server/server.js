b4f76bde1c575a4a4e8d7141e1f941455b93986f
Justin Gillespie <justingillespie@Justins-MBP.home>
2020-04-25 12:27:51 -0400

    organized server/server.js, removed unused cookie-parser and fetch, removed unused and/or commented out routes

--- server/server.js
+++ server/server.js
@@ -1,50 +1,33 @@
 const express = require('express');
-
-const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-
-const PORT = 3000;
-const app = express();
 const path = require('path');
-const curlRouter = require('./routes/curlRouter.js');
-const fetch = require("node-fetch");
 const fs = require("fs");
+const bodyParser = require('body-parser');
+const curlRouter = require('./routes/curlRouter.js');
 
-// statically serve everything in the dist folder on the route '/dist'
+const app = express();
+const PORT = 3000;
+
+// serving index.html
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, '../src/index.html'));
+});
+
+// statically serve '/dist' with approriate headers
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
-app.get("/test", (req, res) => {
-  console.log("hello");
-  return res.status(200).json({"hi": "there"});
-})
 //parse incoming request body
 app.use(bodyParser.json());
 
 
+//route handlers
+app.use('/server', curlRouter);
+
+// getting mock data
 app.get("/local", (req, res) => {
   architecture = fs.readFileSync(path.resolve(__dirname, "./clusterArchitecture.json"),"utf8");
   return res.status(200).json(architecture);
 })
 
-//route handlers
-app.use('/server', curlRouter);
-
-// setInterval(() => {
-//   fetch("http://localhost:3000/server/dev")
-//   .then(result => result.json())
-//   .then(json => console.log(json))
-// }, 60000);
-
-
-// // catch-all route handler for any requests to an unknown route
-// app.get('*', (req, res) => {
-//   console.log(res.status);
-//   return res.status(404).json('Page not found');
-// });
-// root, send index.html
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.resolve(__dirname, '../src/index.html'));
-});
 
 // global error handler
 app.use((err, req, res, next) => {
