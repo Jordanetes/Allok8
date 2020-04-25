197332db0d9cae5114f7574ab87089dcfca7d935
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-04-25 11:18:17 -0400

    Added new route; overview

--- app/routes/curlRouter.ts
+++ app/routes/curlRouter.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 const { getNodesUsage, getPodsUsage, getNodeInfo, getPodInfo, structureData } = require('../controllers/curlController');
 const cleaner = require("../controllers/apiclean");
 const dbEntry = require("../controllers/dbEntry");
-const { dbInformation, cleanOutput } = require('../controllers/dbGet');
+const { dbInformation, cleanOutput, architecture, overview } = require('../controllers/dbGet');
 const curlRouter = express.Router();
 
 curlRouter.post('/getInfo', getPodsUsage, getNodesUsage, getPodInfo, getNodeInfo, structureData, (req: Request, res: Response) => {
@@ -15,6 +15,10 @@ curlRouter.post('/getInfo', getPodsUsage, getNodesUsage, getPodInfo, getNodeInfo
 curlRouter.get('/dev', dbEntry.getAPI, getPodsUsage, getNodesUsage, getPodInfo, getNodeInfo, structureData, cleaner.getNodes, cleaner.nodeMetrics, cleaner.podClean, dbEntry.addNode, (req: Request, res: Response) => {
   return res.status(200).json(res.locals.nodeData);
 });
+
+curlRouter.get('/overview', architecture, overview, (req: Request, res: Response) => {
+  return res.status(200).json(res.locals.clusterOverview);
+})
 
 curlRouter.get('/dbInfo', dbInformation, cleanOutput, (req: Request, res: Response) => {
   return res.status(200).json(res.locals.cleanedOuput);
