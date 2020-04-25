3ee0757f24375932548b7cd562d02c3c67ebefab
Justin Gillespie <justingillespie@Justins-MBP.home>
2020-04-25 13:16:56 -0400

    organized curlRouter and made more readable, left commented out code in for now

--- server/routes/curlRouter.js
+++ server/routes/curlRouter.js
@@ -1,20 +1,38 @@
 const express = require('express');
+const curlRouter = express.Router();
 
-const { getNodesUsage, getPodsUsage, getNodeInfo, getPodInfo, structureData } = require('../controllers/curlController');
 const cleaner = require("../controllers/apiclean");
 const dbEntry = require("../controllers/dbEntry");
 const dbGet = require("../controllers/dbGet");
+const { getNodesUsage, getPodsUsage, getNodeInfo, getPodInfo, structureData } = require('../controllers/curlController');
 
-const curlRouter = express.Router();
 
-curlRouter.post('/getInfo', getPodsUsage, getNodesUsage, getPodInfo, getNodeInfo, structureData, dbEntry.addAPI,(req, res, next) => {
-  res.locals.all = {
+curlRouter.post('/getInfo',
+  getPodsUsage,
+  getNodesUsage, 
+  getPodInfo, 
+  getNodeInfo, 
+  structureData, 
+  dbEntry.addAPI,
+  (req, res, next) => {
+    res.locals.all = {
     nodeInfo: res.locals.nodeInfo,
   }
   return res.status(200).json(res.locals.all);
 });
-curlRouter.get('/dev', dbEntry.getAPI, getPodsUsage, getNodesUsage, getPodInfo, getNodeInfo, structureData, 
-                cleaner.getNodes, cleaner.nodeMetrics, cleaner.podClean, dbEntry.addNode, (req, res) => {
+
+curlRouter.get('/dev', 
+  dbEntry.getAPI, 
+  getPodsUsage, 
+  getNodesUsage, 
+  getPodInfo, 
+  getNodeInfo, 
+  structureData, 
+  cleaner.getNodes, 
+  cleaner.nodeMetrics, 
+  cleaner.podClean, 
+  dbEntry.addNode, 
+  (req, res, next) => {
   // res.locals.all = {
   //   nodes: res.locals.nodes,
   //   nodeMetrics: res.locals.nodeMetricsFormat,
@@ -22,14 +40,22 @@ curlRouter.get('/dev', dbEntry.getAPI, getPodsUsage, getNodesUsage, getPodInfo, 
   // }
   return res.status(200).json(res.locals.nodeData);
 });
-curlRouter.get('/overview', dbGet.architecture, dbGet.overview, (req, res) => {
+
+curlRouter.get('/overview', 
+  dbGet.architecture, 
+  dbGet.overview, 
+  (req, res) => {
   return res.status(200).json(res.locals.clusterOverview);
-})
-curlRouter.get('/dbInfo', dbGet.dbInformation, dbGet.cleanOutput, (req, res) => {
+});
+
+curlRouter.get('/dbInfo', 
+  dbGet.dbInformation, 
+  dbGet.cleanOutput, 
+  (req, res) => {
   // console.log(res.locals.dbResults)
   // return res.status(200).json(res.locals.orderedOutput);
   return res.status(200).json(res.locals.cleanedOutput);
-})
+});
 
 
 module.exports = curlRouter;
